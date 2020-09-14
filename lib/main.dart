fb88f497aa5b8992ceca817f80cbc67a62337e73
rupal-mathur1 <70269196+rupal-mathur1@users.noreply.github.com>
2020-09-14 18:12:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:food/Login%20Page.dart';
+import 'package:flutter_app1/loginpage.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Home(),
-    routes: <String, WidgetBuilder>{
-        '/Login Page ': (context) => LoginPage()
-    },
-  ));
-}
+  runApp(MyApp());
+    }
 
-class Home extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.indigoAccent,
-        elevation: 0.0,
-      ),
-      body: Column(
-        children: <Widget> [
-          CategorySelector(),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(40.0),
-                 topRight: Radius.circular(50.0),
-              ),
-            ),
-              ),
-       ]
-          ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Loginpage(),
     );
   }
 }
-class CategorySelector extends StatefulWidget {
-  @override
-  _CategorySelectorState createState() => _CategorySelectorState();
-}
-
-class _CategorySelectorState extends State<CategorySelector> {
-  int selectedIndex =0;
-  final List<String> categories = ['DRINK','PIZZA','BURGER', 'SANDWICH','ICE-CREAM'];
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100.0,
-      color: Colors.indigoAccent ,
-         );
-  }
-}
-
-
-
